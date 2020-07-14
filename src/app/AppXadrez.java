17448671319348ed23d5e2096254bdc69e6672b9
Fritche <fritche@gmail.com>
2020-07-14 14:45:07 -0300

    1

--- src/app/AppXadrez.java
+++ src/app/AppXadrez.java
@@ -1,9 +1,13 @@
 package app;
 
+import boardgame.Position;
+
 public class AppXadrez {
 
 	public static void main(String[] args) {
 		System.out.println("tst");
+		Position pos = new Position(3,5);
+		System.out.println(pos);
 
 	}
 
