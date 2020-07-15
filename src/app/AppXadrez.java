12423802b042990a06558e5123f7f4163fcbe7a4
Fritche <fritche@gmail.com>
2020-07-15 15:29:31 -0300

    ss2

--- src/app/AppXadrez.java
+++ src/app/AppXadrez.java
@@ -1,6 +1,8 @@
 package app;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -17,10 +19,11 @@ public class AppXadrez {
 		// Board board = new Board(8,8);
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
 		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces());
+				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.println("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
@@ -33,6 +36,9 @@ public class AppXadrez {
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				if(capturedPiece!=null) {
+					captured.add(capturedPiece);
+				}
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
